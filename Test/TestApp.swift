d0c06200ff20eca535d5f6ed1d0d68c1b13ce56a
Xun Ruan <galaxymacos@gmail.com>
2021-11-17 22:35:32 +0800

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Xun Ruan on 2021-11-17.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
